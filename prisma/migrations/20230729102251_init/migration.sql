f8ffded034b1381da55125ab49029ae837931212
Bosco Lau <lth181111@twghlthlp.edu.hk>
2023-07-29 18:26:52 +0800

    migrated to postgres database

--- prisma/migrations/20230729102251_init/migration.sql
+++ prisma/migrations/20230729102251_init/migration.sql
@@ -1,9 +1,11 @@
 -- CreateTable
 CREATE TABLE "Users" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "username" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "todos" TEXT NOT NULL
+    "todos" TEXT NOT NULL,
+
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
